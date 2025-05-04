034d1c4ba1e84c43cc775546b0622d701b10e10a
Prajwol Devkota <prajwol.devkota016@gmail.com>
2025-05-04 15:40:59 +0200

    Responsive navbar added

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,14 +5,17 @@ export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
   const navigate = useNavigate();
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null);
   const [isSeller, setIsSeller] = useState(false);
+  const [showUserLogin, setShowUserLogin] = useState(false);
   const value = {
     navigate,
     user,
     setUser,
     isSeller,
     setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
   };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
