d03b067df7d2439f0294fb6847b053c3bc03653d
Prajwol Devkota <prajwol.devkota016@gmail.com>
2025-05-04 12:29:58 +0200

    initial setup

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,12 @@
+import { createContext, useContext } from "react";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+  const value = {};
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
