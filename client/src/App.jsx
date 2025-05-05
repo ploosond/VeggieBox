9659325ee08f7028e02ac2c6678538e41f55df99
Prajwol Devkota <prajwol.devkota016@gmail.com>
2025-05-05 20:59:41 +0200

    added footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,13 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { Routes, Route, useLocation } from "react-router";
 import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
   return (
     <div>
-      {isSellerPath ? null : <Navbar />}
+      {!isSellerPath && <Navbar />}
       <Toaster />
       <div
         className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
@@ -17,6 +18,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
+      {!isSellerPath && <Footer />}
     </div>
   );
 };
