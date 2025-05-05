9fe1cc7ca7b4903d44a7e55ee4541abbc9fdf333
Prajwol Devkota <prajwol.devkota016@gmail.com>
2025-05-05 19:00:31 +0200

    added MainBanner, Categories, BestSeller, and ProductCart

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,13 +1,63 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
+  const currency = import.meta.VITE_CURRENCY;
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
   const [isSeller, setIsSeller] = useState(false);
   const [showUserLogin, setShowUserLogin] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState({});
+
+  // Fetch all products
+  const fetchProducts = async () => {
+    setProducts(dummyProducts);
+  };
+
+  // Add product to cart
+  const addToCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+
+    if (cartData[itemId]) {
+      cartData[itemId] += 1;
+    } else {
+      cartData[itemId] = 1;
+    }
+
+    setCartItems(cartData);
+    toast.success("Added to Cart");
+  };
+
+  // Update Cart Item Quantity
+  const updateCartItem = (itemId, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId] = quantity;
+    setCartItems(cartData);
+    toast.success("Cart Updated");
+  };
+
+  // Remove Product from Cart
+  const removeFromCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      cartData[itemId] -= 1;
+      if (cartData[itemId === 0]) {
+        delete cartData[itemId];
+      }
+    }
+    setCartItems(cartData);
+    toast.success("Removed from Cart");
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   const value = {
     navigate,
     user,
@@ -16,6 +66,12 @@ export const AppContextProvider = ({ children }) => {
     setIsSeller,
     showUserLogin,
     setShowUserLogin,
+    products,
+    currency,
+    cartItems,
+    addToCart,
+    updateCartItem,
+    removeFromCart,
   };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
