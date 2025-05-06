3b36cd1e12c6c4a6fc436cdded0f5d0fdee74345
Prajwol Devkota <prajwol.devkota016@gmail.com>
2025-05-06 19:31:04 +0200

    added login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,16 @@ import Home from "./pages/Home";
 import { Routes, Route, useLocation } from "react-router";
 import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
+import Login from "./components/Login";
+import { useAppContext } from "./context/AppContext";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
+  const { showUserLogin } = useAppContext();
   return (
     <div>
       {!isSellerPath && <Navbar />}
+      {showUserLogin && <Login />}
       <Toaster />
       <div
         className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
