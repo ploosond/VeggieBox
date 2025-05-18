03dcd9beab1570623c3c281f4828f1ea164bf919
Prajwol Devkota <prajwol.devkota016@gmail.com>
2025-05-18 19:26:07 +0200

    added orders router

--- server/app.js
+++ server/app.js
@@ -3,15 +3,16 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import connectDB from './configs/db.js';
 import 'dotenv/config';
+
 import userRouter from './routes/userRoute.js';
 import sellerRouter from './routes/sellerRoute.js';
 import connectCloudinary from './configs/cloudinary.js';
 import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import addressRouter from './routes/addressRoute.js';
+import orderRouter from './routes/orderRoute.js';
 
 const app = express();
-const port = process.env.PORT || 4000;
 
 await connectDB();
 
@@ -34,7 +35,6 @@ app.use('/api/seller/', sellerRouter);
 app.use('/api/product/', productRouter);
 app.use('/api/cart/', cartRouter);
 app.use('/api/address/', addressRouter);
+app.use('/api/order/', orderRouter);
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+export default app;
--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -27,6 +27,35 @@ export const placeOrderCOD = async (req, res) => {
         console.log(error.message);
         return res.json({success: false, message: error.message});
     }
-
 };
 
+// Get Orders by User ID: /api/order/user
+
+export const getUserOrders = async (req, res) => {
+    try {
+        const {userId} = req;
+        const orders = await Order.find({
+            userId,
+            $or: [{paymentType: 'COD'}, {isPaid: true}]
+        }).populate('items.product address').sort({createdAt: -1});
+        return res.json({success: true, orders});
+    } catch (error) {
+        console.log(error.message);
+        return res.json({success: false, message: error.message});
+    }
+};
+
+// Get All Orders ( for seller / admin ) : /api/order/seller
+
+export const getAllOrders = async (req, res) => {
+    try {
+
+        const orders = await Order.find({
+            $or: [{paymentType: 'COD'}, {isPaid: true}]
+        }).populate('items.product address').sort({createdAt: -1});
+        return res.json({success: true, orders});
+    } catch (error) {
+        console.log(error.message);
+        return res.json({success: false, message: error.message});
+    }
+};
